1600e1fac8f5cc6a0454f6e8f23725821e159a03
ayush20152015 <111439287+ayush20152015@users.noreply.github.com>
2024-12-03 17:56:30 +0000

    dates and time in js

---
+++ 1_basics/dates.js
@@ -0,0 +1,53 @@
+let mydate=new Date();
+
+//console.log(typeof mydate);
+
+//console.log(mydate.toString());
+//console.log(mydate.toLocaleString());
+
+//let myCreatedDate=new Date(2023,0,23,5,3);
+
+//console.log(myCreatedDate.toLocaleString());
+
+//console.log(myCreatedDate.toDateString());
+
+//let mynewdate= new Date("01-14-2023 5:00 PM ");
+
+//console.log(mynewdate.toLocaleString());
+
+//let mytimestamp=Date.now();
+
+//console.log(mytimestamp);
+
+//console.log(mydate.getDay());
+
+//console.log(Math.floor(mytimestamp/1000)); 
+
+// ms in seconds
+
+let newDate =new Date();
+console.log(newDate.getMonth()+1);
+console.log(newDate.getDay());
+
+
+newDate.toLocaleString('default',{
+
+      weekday: "long",
+      day:"short",
+
+ 
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
