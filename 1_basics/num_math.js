94c7d731ca240f56315a070b056e466c714efaa1
ayush20152015 <111439287+ayush20152015@users.noreply.github.com>
2024-12-03 16:40:10 +0000

    random function in math

--- 1_basics/num_math.js
+++ 1_basics/num_math.js
@@ -1,33 +1,49 @@
-//const score =400;
-console.log(400);
+////const score =400;
+//console.log(400);
+//
+//let score =new Number(400);
+//
+//console.log(score);
+//
+//console.log(score.toFixed(4));   // prefer this one 
+//
+//const score2=123.000045
+//
+//console.log(score2.toPrecision(8));   
+//
+//
+//console.log(score2.toLocaleString());
+//
+//let val=1000000000000000;
+//
+//console.log(val.toLocaleString('en-US'));
+//
+//// *****************maths******************//
+//
+//console.log(Math);
+//
+//
+//console.log(Math.max(1,2,3,4,5));
+//
+//console.log(Math.ceil(4.2));
+//console.log(Math.floor(4.6));
+//console.log(Math.abs(-44));
+ 
+//console.log(Math.random());  // genrate the random value between 0 and 1
 
-let score =new Number(400);
 
-console.log(score);
+for(let i=0;i<100;i++){
+     
+    const min=10;
+const max=20;
 
-console.log(score.toFixed(4));   // prefer this one 
-
-const score2=123.000045
-
-console.log(score2.toPrecision(8));   
+console.log(Math.floor(Math.random()*(max-min+1))+min);   // genrate integer value between min and max
 
 
-console.log(score2.toLocaleString());
 
-let val=1000000000000000;
+      
+}
 
-console.log(val.toLocaleString('en-US'));
-
-// *****************maths******************//
-
-console.log(Math);
-
-
-console.log(Math.max(1,2,3,4,5));
-
-console.log(Math.ceil(4.2));
-console.log(Math.floor(4.6));
-console.log(Math.abs(-44));
 
 
 
