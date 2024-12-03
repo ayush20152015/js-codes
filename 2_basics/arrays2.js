7810a3404252ffc4faf9e03b5bf47339434b26a1
ayush20152015 <111439287+ayush20152015@users.noreply.github.com>
2024-12-03 20:05:50 +0000

    other operations in arrays

---
+++ 2_basics/arrays2.js
@@ -0,0 +1,37 @@
+const marvel=["a","b","c","d"];
+const dc=["c","h","d"];
+
+//dc.push(marvel);    push the data inside it
+
+//console.log(dc);    
+
+
+//console.log(dc[3][2]);
+
+//total=dc.concat(marvel);
+
+//console.log(total);
+
+// spread function is used to join arrays
+
+//const myn=[...marvel,...dc];
+
+//console.log(myn);
+
+//const array=[1,2,3,[1,2,3,[5,6,7]]];
+
+//const array2=array.flat(Infinity);   
+
+//console.log(array2);
+
+//console.log(Array.isArray("ayush"));  // return boolean value
+ 
+console.log(Array.from("ayush"));      // forms arrays from the data
+
+let t=400;
+let y=500;
+  
+console.log(Array.of(t,y));   // make arrays of objects
+
+
+
