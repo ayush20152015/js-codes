4aa548b378ffef021e3939757701254505c1f4f7
ayush20152015 <111439287+ayush20152015@users.noreply.github.com>
2024-12-03 06:36:36 +0000

    changes done

---
+++ 1_basics/data_types.js
@@ -0,0 +1,31 @@
+"use strict"  // use newer version of js 
+
+//alert("hello") // we are using nodejs not webrowser
+
+console.log("hi") ; console.log("hello");  // error if semicolon is not given in between 
+
+console.log(3+
+    3)  // code readanility is poor
+
+
+    // datatypes in js
+  
+  let name="ayush";
+  let roll="18";
+  
+  let isloggedin=false;
+
+  let state;
+
+  // string ""
+  // number range till (1<<53)
+  // bigint
+  // boolean 
+  // undefined
+  // null (object)
+  // object
+  // undefined
+
+console.log(typeof "ayush");
+console.log(typeof null);
+console.log(typeof undefined);
--- 1_basics/test.js
+++ 1_basics/test.js
@@ -1,1 +1,27 @@
-console.log("hello")
+const i="1234"   // assing cosntant value to something and redeclaration is not possible
+
+console.log(i);   // print one value at a time 
+
+var name ="12345";    // warning not to use as it  has issues with block spaces and functional spaces
+
+accountname="ayush choudhary"; // js allow you to assing memory without assinging the variable type its a wrong practice but doable
+
+let add="1234556676777";      // let is used to assing but changable value 
+
+console.table([i,accountname,add]);
+
+let  add2;   // undefined
+
+
+console.table([i,accountname,add,add2]);
+
+
+
+
+
+
+ 
+
+ 
+
+
