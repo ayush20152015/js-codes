058b19929395494363cd6ac77ef0806038a59c70
ayush20152015 <111439287+ayush20152015@users.noreply.github.com>
2024-12-03 16:24:00 +0000

    maths and num in js

---
+++ 1_basics/num_math.js
@@ -0,0 +1,35 @@
+//const score =400;
+console.log(400);
+
+let score =new Number(400);
+
+console.log(score);
+
+console.log(score.toFixed(4));   // prefer this one 
+
+const score2=123.000045
+
+console.log(score2.toPrecision(8));   
+
+
+console.log(score2.toLocaleString());
+
+let val=1000000000000000;
+
+console.log(val.toLocaleString('en-US'));
+
+// *****************maths******************//
+
+console.log(Math);
+
+
+console.log(Math.max(1,2,3,4,5));
+
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.6));
+console.log(Math.abs(-44));
+
+
+
+
+
