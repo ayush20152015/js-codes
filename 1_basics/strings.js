554168d7de816a891aa80095a13c36a114b26d95
ayush20152015 <111439287+ayush20152015@users.noreply.github.com>
2024-12-03 15:44:28 +0000

    strings and its properties in js

---
+++ 1_basics/strings.js
@@ -0,0 +1,59 @@
+let str1="abc";
+let str2=50;
+
+//console.log(50+str1);
+
+//console.log(str1+" value "+str2);   // outdated method to print 
+//
+//console.log(`hello my name is ${str1} and my roll number is ${str2}`);
+//
+//const gamename= new String("mariocontra");
+//
+//console.log(gamename);
+//
+//
+//console.log(gamename.length);
+//
+//const ch=gamename.charAt(2);
+
+//console.log(ch);
+
+//console.log(gamename.toUpperCase(gamename));
+
+
+//console.log(gamename.indexOf('r'));
+
+//console.log(gamename.substring(0,5));   // exclude final value and cannot take negative values
+ 
+//console.log(gamename.slice(-7,4));       // can take negative values and start from end 
+
+
+const anothername="   ayushchoudhary";
+
+console.log(anothername.trim());
+
+console.log(anothername.trimEnd());
+
+url ="http://myspace.blogspot%20%.com";
+
+url=url.replace("%20%",'');
+
+console.log(url);
+
+console.log(str1.split('-'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
