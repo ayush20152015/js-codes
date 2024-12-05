5032b8b824a6a333eeb5f5199e3b663a81e21992
ayush20152015 <111439287+ayush20152015@users.noreply.github.com>
2024-12-05 20:32:53 +0000

    mini-hoisting in js

--- 2_basics/scope_namespaces.js
+++ 2_basics/scope_namespaces.js
@@ -26,16 +26,26 @@ const x=40;
 
 // problem with var
 
-var d=500;
+//var d=500;
 
-if(true){
+//if(true){
 
-    var d=30;
-}
+ //   var d=30;
+//}
 
-console.log(d);  // suppose to be 500 but its value is changed in localnamespace which must not suppose to happen
+//console.log(d);  // suppose to be 500 but its value is changed in localnamespace which must not suppose to happen
 
 // the values in global namespace is available in local namespace but the value for local namespace does not affect the values in global one
+
+console.log(addtwo(3));
+
+function addtwo(num){
+
+     return num+2;
+}     // but this will work 
+
+
+//const vl=addtwo(3);  // function is declared before it can be accessed so it is hoisting 
 
 
 
