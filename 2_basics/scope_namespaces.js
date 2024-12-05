595b151c2e4978f291961266b9414d3f1fe0510c
ayush20152015 <111439287+ayush20152015@users.noreply.github.com>
2024-12-05 20:08:27 +0000

    scopes and namespace in js

---
+++ 2_basics/scope_namespaces.js
@@ -0,0 +1,43 @@
+let a=10;
+let b=20;
+let c=30;
+
+//console.log(a);
+//console.log(b);
+//console.log(c);
+
+{
+    //a=20;   // this a is not same as above a 
+
+      
+
+    //console.log(a);
+
+      
+}
+
+const x=40;
+
+{
+     // x=30;
+
+      //console.log(x);  // give error as value cannot be changed for const type
+}
+
+// problem with var
+
+var d=500;
+
+if(true){
+
+    var d=30;
+}
+
+console.log(d);  // suppose to be 500 but its value is changed in localnamespace which must not suppose to happen
+
+// the values in global namespace is available in local namespace but the value for local namespace does not affect the values in global one
+
+
+
+
+
